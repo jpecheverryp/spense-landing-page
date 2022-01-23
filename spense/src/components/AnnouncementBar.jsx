c9f56d1b19c63880d012839ecc87369fddfe56b7
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-01-23 14:14:54 -0500

    add Announcement bar content

---
+++ spense/src/components/AnnouncementBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AnnouncementBar = () => {
+  return (
+    <div className='announcement-bar'>
+      <p>
+        To celebrate Spense's launch, we will be waiving all fees for the entire
+        month of April. You will be receiving 100% fo the earnings
+      </p>
+    </div>
+  );
+};
+
+export default AnnouncementBar;

4100caf9cf51e7eea870c65864e385826e8fbc5d
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-01-23 16:31:20 -0500

    styled navbar

--- spense/src/components/Navbar.jsx
+++ spense/src/components/Navbar.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>spense</div>
-      <ul>
-        <li>Business</li>
-        <li>Pricing</li>
-        <li>Features</li>
-        <li>Login</li>
-        <li>Get Started</li>
+    <nav className='navbar'>
+      <span className='navbar__brand'>spense.</span>
+      <ul className='navbar__pages navbar__links'>
+        <li className='navbar__item'>Business</li>
+        <li className='navbar__item'>Pricing</li>
+        <li className='navbar__item'>Features</li>
+      </ul>
+      <ul className='navbar__login navbar__links'>
+        <li className='navbar__item'>Login</li>
+        <li className='navbar__item navbar__register'>Get Started</li>
       </ul>
     </nav>
   );

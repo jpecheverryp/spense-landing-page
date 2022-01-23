80d29d90f77a247e927b039e3c085b8906fdd439
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-01-23 14:26:36 -0500

    add hero component

--- spense/src/App.js
+++ spense/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import AnnouncementBar from './components/AnnouncementBar';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='App'>
       <AnnouncementBar />
       <Navbar />
+      <Hero />
     </div>
   );
 }

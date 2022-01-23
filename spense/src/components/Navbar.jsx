69ad8588b9bd3fbad406e26c27fb8a7663f5228f
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-01-23 14:15:42 -0500

    add navbar content

---
+++ spense/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>spense</div>
+      <ul>
+        <li>Business</li>
+        <li>Pricing</li>
+        <li>Features</li>
+        <li>Login</li>
+        <li>Get Started</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
